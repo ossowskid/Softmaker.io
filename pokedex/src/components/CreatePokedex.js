9c3202081e587013ee02df380c21d930c045b263
ossowskid <ossowski.dawid@gmail.com>
2021-11-09 21:06:45 +0100

    wip

--- pokedex/src/components/CreatePokedex.js
+++ pokedex/src/components/CreatePokedex.js
@@ -1,23 +1,33 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { NavLink, Outlet } from "react-router-dom";
 import { useGetPokemon } from "../hooks/useGetPokemon";
 
 export const CreatePokedex = () => {
   const [filterData, setFilterData] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
   const pokemons = useGetPokemon();
 
-  const handleChange = (e) => {
-    // e.preventDefault();git
+  useEffect(() => {
+    setSuggestions(pokemons);
+  }, [pokemons]);
+
+  const handleSuggestions = (e) => {
     setFilterData(e.target.value);
-    console.log(e.target.value);
-    return filterData;
+    const matches = suggestions.filter((el) => {
+      const regex = new RegExp(`^${e.target.value}`, "gi");
+      return el.name.match(regex);
+    });
+    setSuggestions(matches);
+    if (e.target.value.length === 0) {
+      setSuggestions(pokemons);
+    }
   };
 
   return (
     <div className="App" style={{ textAlign: "center" }}>
-      <input type="text" onChange={handleChange} />
+      <input type="text" value={filterData} onChange={handleSuggestions} />
 
-      {pokemons.map((el, i) => {
+      {suggestions.map((el, i) => {
         return (
           <div key={i}>
             <NavLink to={`pokemons/${el.name}`}>{el.name}</NavLink>
